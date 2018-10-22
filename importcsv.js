1b8bd3d007e3f991185cbd6163ed524bf10a6d60
andrkyn <andrhil14@gmail.com>
2018-10-22 13:57:26 +0300

    added json-object

--- importcsv.js
+++ importcsv.js
@@ -1,8 +1,13 @@
 var csv=require('csvtojson');
 
 var csvFilePath='./athlete_events.csv';
+
+//added json-object for loading csv-files into it
 csv()
     .fromFile(csvFilePath)
     .then((json)=>{
         console.log(json);
-});
+
+
+    });
+
