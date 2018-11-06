59dbc7339cda7d7be652ee305ff6d1bbfbe4a037
andrkyn <andrhil14@gmail.com>
2018-11-07 00:33:49 +0200

    error correction and optimization

--- stat.js
+++ stat.js
@@ -12,54 +12,63 @@ param[1] = process.argv[3];
 param[2] = process.argv[4];
 
 var query = '';
-var arrNoc = [], numMedal = [], arrSeason = [], arrYear = [];
-var strNoc2;
-var strYear2;
-var fndNoc = 0;
-var fndYear = 0;
+var numMedal = [], arrSeason = [], arrParam = [];
+var strParam2;
+var fndParam = 0;
 var endCycle = 0;
 var alg = 0;
 
-for (var i = 0; i < process.argv.length; i++) {
-    if (!isNaN(param[i])) {
-        console.log('это число');
-        query = query + 'and year=?';
+for (var j = 0; j < process.argv.length; j++) {
+    if (param[j] == 3 && isNaN(param[j])) {
+        alg = 1;
+    } else {
         alg = 2;
+    }
+}
+
+for (var i = 0; i < process.argv.length; i++) {
+
+
+    if (!isNaN(param[i])) {
+        query = query + 'and year=?';
     }
 
     if ((param[i] == 'summer' || param[i] == 'winter')) {
         param[i] = param[i].replace(/summer/g, "0");
         param[i] = param[i].replace(/winter/g, "1");
         query = query + 'and season=?';
-        alg = 2;
-    }
+
+    }/*else{
+     console.log('\x1b[36m', 'not found in database');
+     }*/
 
     if ((param[i] == 'gold' || param[i] == 'silver' || param[i] == 'bronze') && (!param[i].match(/^\d+/))) {
         param[i] = param[i].replace(/gold/g, "1");
         param[i] = param[i].replace(/silver/g, "2");
         param[i] = param[i].replace(/bronze/g, "3");
         query = query + 'and medal=?';
-        alg = 2;
-    } else {
-        console.error('Not parametr');
-    }
+
+    }/*else{
+        console.log('\x1b[36m', 'not found in database');
+    }*/
 
     var len = 0;
     if (typeof param[i] !== 'undefined') len = param[i].length;
+
     if ((len == 3) && param[i] !== '') {
         param[i] = param[i].toUpperCase();
         query = query + 'and noc_name=?';
         alg = 1;
     }
 }
-console.log('alg: ' + alg);
+
 function test(arg1, arg2, arg3, callback) {
     db.serialize(function () {
         db.all(" SELECT * from athletes,teams,results,games WHERE results.game_id=games.id" +
             " and athletes.team_id=teams.id and results.id=athletes.id " + query,
-            [arg1, arg2, arg3], function (err, rows,) {
+            [arg1, arg2, arg3], function (err, rows) {
                 if (!err) {
-                    console.log(rows.length);
+                    //console.log(rows.length);
                     rows.forEach(function (row) {
                         //console.log(row.game_id);
                         numCol++;
@@ -83,123 +92,68 @@ function test(arg1, arg2, arg3, callback) {
                 }
             });
         db.close();
-        console.log('Close the database connection.');
+        //console.log('Close the database connection.');
     });
 }
 
-/// for medal
-if (alg == 1) {
-    test(param[0], param[1], param[2], function (a, b, c, d) {
-        arr[0] = a;
-        arr[1] = b;
-        arr[2] = c;
-        arr[3] = d;
+// for top comand and medal
+test(param[0], param[1], param[2], function (a, b, c, d) {
+    arr[0] = a;
+    arr[1] = b;
+    arr[2] = c;
+    arr[3] = d;
 
-        var strNoc = arr[2];
-        var strSeason = arr[3];
-        var kolMedal = arr[0];
-        var posNoc = arrNoc.indexOf(strNoc) == -1;
+    if (alg == 1) { var strParam = arr[2]; }
+    if (alg == 2) { var strParam = arr[1]; }
 
+    var strSeason = arr[3];
+    var kolMedal = arr[0];
+    var posParam = arrParam.indexOf(strParam) == -1;
 
-        if (posNoc == true) {
-            arrNoc.push(strNoc);
-            arrSeason.push(strSeason);
+    if (posParam == true) {
+        arrParam.push(strParam);
+        arrSeason.push(strSeason);
+    }
+
+    fndParam = arrParam.findIndex(indCity);
+    function indCity(element) {
+        return element == strParam;
+    }
+
+    if (numMedal[fndParam] == undefined) {
+        numMedal[fndParam] = 0;
+    }
+
+    for (var i = 0; i <= arrParam.length; i++) {
+        if ((kolMedal.valueOf() > 0) && i == arrParam.length) {
+            numMedal[fndParam] = numMedal[fndParam] + 1;
         }
+    }
+    strParam2 = strParam;
 
-        fndNoc = arrNoc.findIndex(indCity);
-        function indCity(element) {
-            return element == strNoc;
-        }
+    if (endCycle == 1) {
 
-        if (numMedal[fndNoc] == undefined) {
-            numMedal[fndNoc] = 0;
-        }
+        var progress = '';
+        var str;
 
-        for (var i = 0; i <= arrNoc.length; i++) {
-            if ((kolMedal.valueOf() > 0) && i == arrNoc.length) {
-                numMedal[fndNoc] = numMedal[fndNoc] + 1;
-            }
-        }
-        strNoc2 = strNoc;
-
-        if (endCycle == 1) {
-            var progre = '';
-            var str;
+        if (alg == 1) {
             console.log('------------ Medal--------------');
-            console.log('Season  Year');
-
-            for (var i = 0; i <= arrNoc.length - 1; i++) {
-                if (numMedal[i] > 0) {
-                    for (var j = 0; j < numMedal[i]; j++) {
-                        progre = progre + '█|';
-                    }
-                }
-                numMedal[i] = progre;
-                progre = '';
-                console.log(arrSeason[i] + '  ' + arrNoc[i] + '  ' + numMedal[i]);
-
-            }
-
+            console.log('>Season<  >Year<');
         }
-    });
-}
-
-// for top comand
-if (alg == 2) {
-    test(param[0], param[1], param[2], function (a, b, c, d) {
-        arr[0] = a;
-        arr[1] = b;
-        arr[2] = c;
-        arr[3] = d;
-
-        var strYear = arr[1];
-        var strSeason = arr[3];
-        var kolMedal = arr[0];
-        var posYear = arrYear.indexOf(strYear) == -1;
-
-
-        if (posYear == true) {
-            arrYear.push(strYear);
-            arrSeason.push(strSeason);
-        }
-
-        fndYear = arrYear.findIndex(indCity);
-        function indCity(element) {
-            return element == strYear;
-        }
-
-        if (numMedal[fndYear] == undefined) {
-            numMedal[fndYear] = 0;
-        }
-
-        for (var i = 0; i <= arrYear.length; i++) {
-            if ((kolMedal.valueOf() > 0) && i == arrYear.length) {
-                numMedal[fndYear] = numMedal[fndYear] + 1;
-            }
-        }
-        strYear2 = strYear;
-
-        ///////////////////////////////////////
-        if (endCycle == 1) {
-            var progre = '';
-            var str;
+        if (alg == 2) {
             console.log('------------ Top comand--------------');
-            console.log('Season  Year');
-
-            for (var i = 0; i <= arrYear.length - 1; i++) {
-                if (numMedal[i] > 0) {
-                    for (var j = 0; j < numMedal[i]; j++) {
-                        progre = progre + '█|';
-                    }
-                }
-                numMedal[i] = progre;
-                progre = '';
-                console.log(arrSeason[i] + '  ' + arrYear[i] + '  ' + numMedal[i]);
-
-            }
-
+            console.log('>Season<  >Comand<');
         }
-    });
-}
 
-//console.log(process.argv.length);
+        for (var i = 0; i <= arrParam.length - 1; i++) {
+            if (numMedal[i] > 0) {
+                for (var j = 0; j < numMedal[i]; j++) {
+                    progress = progress + '█';
+                }
+            }
+            numMedal[i] = progress;
+            progress = '';
+            console.log(' ' + arrSeason[i] + '    ' + arrParam[i] + '  ' + numMedal[i]);
+        }
+    }
+});
